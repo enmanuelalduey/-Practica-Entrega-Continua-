d20b3612e1e35b7e79835d9729910471761333aa
enmanuelalduey <enmanuelaldueysantana21@gmail.com>
2026-08-06 11:52:20 -0400

    practica

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM php:8.2-apache
+
+COPY . /var/www/html/
+
+EXPOSE 80
+
+
+
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Hola Mundo desde PHP con Docker</h1>";
+echo "<p>Practica DevOps - Enmanuel alduey </p>";
+
+
+
+
